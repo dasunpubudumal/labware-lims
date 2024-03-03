1c5be10ca1330c4082ed3154daa3c9ec2fb448d9
Dasun Pubudumal <pubudumald@gmail.com>
2024-03-03 09:44:00 +0000

    sample history table migratiosn

---
+++ app/models/sample_transfer_history.rb
@@ -0,0 +1,2 @@
+class SampleTransferHistory < ApplicationRecord
+end
---
+++ test/models/sample_transfer_history_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SampleTransferHistoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
