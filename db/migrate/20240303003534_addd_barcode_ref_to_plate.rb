e69d3adde321cebb28f4136b155dab6bfdd5e297
Dasun Pubudumal <pubudumald@gmail.com>
2024-03-03 09:10:25 +0000

    fix foreign key issue with barcode and plates

--- db/migrate/20240303003534_addd_barcode_ref_to_plate.rb
+++ db/migrate/20240303003534_addd_barcode_ref_to_plate.rb
@@ -1,13 +1,11 @@
 class AdddBarcodeRefToPlate < ActiveRecord::Migration[7.1]
   def up
-    add_column :plates, :barcode, :string
-    add_foreign_key :plates, :barcodes, column: :plate_barcode, primary_key: :barcode, name: 'barcode'
-    add_index :plates, :barcode
+    add_foreign_key :plates, :barcodes, column: :plate_barcode, primary_key: :barcode, name: 'plate_barcode'
+    add_index :plates, :plate_barcode
   end
 
   def down
-    remove_index :plates, :barcode
+    remove_index :plates, :plate_barcode
     remove_foreign_key :plates, :barcodes, column: :plate_barcode
-    remove_column :plates, :barcode
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_03_010521) do
   create_table "plates", primary_key: "plate_barcode", id: :string, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "barcode"
-    t.index ["barcode"], name: "index_plates_on_barcode"
+    t.index ["plate_barcode"], name: "index_plates_on_plate_barcode"
   end
 
   create_table "samples", primary_key: "sanger_sample_id", id: :string, force: :cascade do |t|
