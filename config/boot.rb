f42a13e4f2c1d9ef711c5a8583414fca5007dfe3
dasunpubudumal <dasun.15@cse.mrt.ac.lk>
2024-03-10 17:24:18 +0000

    Amending issues with EXECJS runtime variable

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# make the ExecJs use NodeJs
+ENV['EXECJS_RUNTIME'] = 'Node'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
